14722b0bc05ee74458a56d1a2aed34f99c3e2a1c
Jordy Aguilar <55517744+houaguilar@users.noreply.github.com>
2023-09-30 19:47:10 -0500

    Initial Commit

---
+++ InstaSwiftUI/InstaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaSwiftUIApp.swift
+//  InstaSwiftUI
+//
+//  Created by Jordy Aguilar on 30/09/23.
+//
+
+import SwiftUI
+
+@main
+struct InstaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
